96a1c4e1f9fa71a55323e44a0415a1f87da81968
seaturtl33 <leo.katsanos@gmail.com>
2026-07-22 14:31:16 +0900

    Finished player aniamtions/movement - Leo K

--- Leo K/player.gd
+++ Leo K/player.gd
@@ -2,24 +2,28 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	process_movement()
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+func process_movement() -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	var direction := Input.get_vector("left", "right", "up", "down")
+	
+	velocity = direction * SPEED
+	
+	play_animation(direction)
+	
+func play_animation(dir: Vector2) -> void:
+	if dir.x != 0:
+		animated_sprite_2d.flip_h
+		animated_sprite_2d.play("run_right")
+	elif dir.y < 0:
+		animated_sprite_2d.play("run_up")
+	elif dir.y > 0:
+		animated_sprite_2d.play("run_down")
