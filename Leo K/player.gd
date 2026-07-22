fc45f3fdcecab3f03e51572dc246e8a04eff9df7
seaturtl33 <leo.katsanos@gmail.com>
2026-07-22 15:26:02 +0900

    idle and running test animations work properly - Leo K

--- Leo K/player.gd
+++ Leo K/player.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+var last_direction: Vector2 = Vector2.RIGHT	
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	process_movement()
+	process_animation()
 	move_and_slide()
 
 func process_movement() -> void:
@@ -15,15 +18,25 @@ func process_movement() -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "up", "down")
 	
-	velocity = direction * SPEED
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
 	
-	play_animation(direction)
 	
-func play_animation(dir: Vector2) -> void:
+func process_animation() -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("run", last_direction)
+	else:
+		play_animation("idle", last_direction)
+	
+	
+func play_animation(prefix: String, dir: Vector2) -> void:
 	if dir.x != 0:
-		animated_sprite_2d.flip_h
-		animated_sprite_2d.play("run_right")
+		animated_sprite_2d.flip_h = dir.x < 0
+		animated_sprite_2d.play(prefix + "_right")
 	elif dir.y < 0:
-		animated_sprite_2d.play("run_up")
+		animated_sprite_2d.play(prefix + "_up")
 	elif dir.y > 0:
-		animated_sprite_2d.play("run_down")
+		animated_sprite_2d.play(prefix + "_down")
