22efcb8498e7f71ad871093d3c72720de111eb16
seaturtl33 <leo.katsanos@gmail.com>
2026-07-24 09:44:18 +0900

    shooting bullets work - Leo K

--- Leo K/player.gd
+++ Leo K/player.gd
@@ -8,6 +8,40 @@ var last_direction: Vector2 = Vector2.RIGHT
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
+@export var cooldown = 0.25
+@export var bullet_scene : PackedScene
+var can_shoot = true
+
+func _ready():
+	start()
+
+func start():
+	#position = Vector2(screensize.x / 2, screensize.y - 64)
+	$GunCooldown.wait_time = cooldown
+
+func shoot():
+	print("Start shoot")
+	if not can_shoot:
+		return
+	can_shoot = false
+	$GunCooldown.start() 
+	
+	print("Spawn bullet")
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start(position + Vector2(0, -8))
+
+func _process(_delta):
+	if Input.is_action_pressed("shoot"):
+	#if Input.is_key_pressed(KEY_Z):
+		shoot()
+		
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
+
+
+
 func _physics_process(_delta: float) -> void:
 	process_movement()
 	process_animation()
