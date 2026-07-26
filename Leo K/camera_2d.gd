faad30b801ec1c737a3f517a08b26421584bdbd3
seaturtl33 <leo.katsanos@gmail.com>
2026-07-26 11:09:20 +0900

    fixed camera, tested tiles - Leo K

---
+++ Leo K/camera_2d.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	#zoom = Vector2(2.9,2.9)
+	pass
