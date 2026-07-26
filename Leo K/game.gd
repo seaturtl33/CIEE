047a2f29db2ef1873679608cebf60d23c15f912e
seaturtl33 <leo.katsanos@gmail.com>
2026-07-26 22:40:59 +0900

    coding umbrella enemy movement and more - Leo K

---
+++ Leo K/game.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var enemy = preload("res://Leo K/umbrella_2.tscn")
+var score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_enemies()
+
+func spawn_enemies():
+	for x in range(9):
+		for y in range(3):
+			var e = enemy.instantiate()
+			var pos = Vector2(x * (16 + 8) + 24, 16 * 4 + y * 16)
+			add_child(e)
+			e.start(pos)
+			e.died.connect(_on_enemy_died)
+
+func _on_enemy_died(value):
+	score += value
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
